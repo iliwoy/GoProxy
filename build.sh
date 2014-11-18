93534ee4d4011b521fa6fc331349617f5b867475
lost <lost@com>
2014-11-18 16:11:28 +0800

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if [ ! -d "src/github.com/djimenez/iconv-go/" ]; then
+mkdir -p src/github.com/djimenez/iconv-go/
+git clone https://github.com/djimenez/iconv-go/ src/github.com/djimenez/iconv-go/
+fi
 export GOPATH=`pwd`
 go build main
